979455bebc120dfccd88cf4178fb131ba499bbf4
Luka Ponikvar <luka.ponikvar1@gmail.com>
2024-09-05 16:11:07 +0200

    Dodala funkcijo expected

---
+++ machine/src/communication/expected.rs
@@ -0,0 +1,14 @@
+use crate::{functions::our_sequences::our_sequences, structs::sequences::SequenceRequest};
+
+pub fn expected(request: &SequenceRequest, name: &String) -> bool {
+    let our_sequences = our_sequences();
+    let index = our_sequences.iter().position(|seq| {
+        seq.name == *name
+            && seq.parameters as usize == request.parameters.len()
+            && seq.sequences as usize == request.sequences.len()
+    });
+    match index {
+        Some(_) => true,
+        None => false,
+    }
+}
