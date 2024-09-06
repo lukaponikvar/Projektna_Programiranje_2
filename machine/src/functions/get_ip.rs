c28c513561d24f54385e78de557af33d29f12b3e
Luka Ponikvar <luka.ponikvar1@gmail.com>
2024-09-06 11:34:39 +0200

    error handling funkcij get_ip, get_port

--- machine/src/functions/get_ip.rs
+++ machine/src/functions/get_ip.rs
@@ -2,19 +2,25 @@ use regex::Regex;
 
 ///Funkcija vzame niz oblike `"X.X.X.X"` in vrne `ip` naslov.
 pub fn get_ip(string: &String) -> [u8; 4] {
-    let reg = Regex::new(r"^(\d*)\.(\d*)\.(\d*)\.(\d*)$").unwrap();
-    let findings = reg.captures(&string).unwrap();
-    let mut ip = Vec::new();
+    let reg = match Regex::new(r"^(\d*)\.(\d*)\.(\d*)\.(\d*)$") {
+        Ok(r) => r,
+        Err(_) => return [127, 0, 0, 1],
+    };
+    let findings = match reg.captures(&string) {
+        Some(f) => f,
+        None => return [127, 0, 0, 1],
+    };
+    let mut ips = Vec::new();
     for i in 1..5 {
-        ip.push(
-            findings
-                .get(i)
-                .unwrap()
-                .as_str()
-                .to_string()
-                .parse()
-                .expect("Failed to parse string to integer"),
-        );
+        let our_match = match findings.get(i) {
+            Some(m) => m,
+            None => return [127, 0, 0, 1],
+        };
+        let ip = match our_match.as_str().to_string().parse() {
+            Ok(r) => r,
+            Err(_) => return [127, 0, 0, 1],
+        };
+        ips.push(ip);
     }
-    [ip[0], ip[1], ip[2], ip[3]]
+    [ips[0], ips[1], ips[2], ips[3]]
 }
