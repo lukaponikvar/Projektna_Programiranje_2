a505f7021555f0287948053f2d40e1545d098203
Luka Ponikvar <luka.ponikvar1@gmail.com>
2024-09-10 12:23:45 +0200

    Dodala hitro potenciranje

--- machine/src/mathematical_functions/power.rs
+++ machine/src/mathematical_functions/power.rs
@@ -2,9 +2,13 @@
 pub fn power(base: f64, exponent: u64) -> f64 {
     if exponent == 0 {
         1.0
+    } else if exponent == 1 {
+        base
+    } else if exponent == 2 {
+        base * base
+    } else if exponent % 2 == 0 {
+        power(base * power(base, exponent / 2), 2)
     } else {
-        base * power(base, exponent - 1)
+        power(base * power(base, exponent / 2), 2) * base
     }
 }
-
-// TODO: hitro potenciranje
