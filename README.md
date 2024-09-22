4cfcb10df9d91451e9c01f781b34d14e6ea1f48b
Luka Ponikvar <luka.ponikvar1@gmail.com>
2024-09-22 12:21:27 +0200

    Dodala zaporedja v readme

--- README.md
+++ README.md
@@ -6,13 +6,94 @@
 
 ## Available sequences
 
->__Arithmetic sequence__  
->_Description:_ 
->Arithmetic sequence which takes two parameters: a start and a step.  
->_Parameters:_ 
-> 2  
->_Sequences:_ 
-> 0
+> __Arithmetic sequence__  
+> _Description:_ 
+> Arithmetic sequence which takes two parameters: a start and a step.  
+> _Parameters:_ 
+>  2  
+> _Sequences:_ 
+>  0
+
+> __Average sequence__  
+> _Description:_ 
+> A sequence that takes two sequences and calculates their average term by term.  
+> _Parameters:_ 
+>  0  
+> _Sequences:_ 
+>  2
+
+> __Constant sequence__  
+> _Description:_ 
+> Constant sequence with a single parameter: a value.  
+> _Parameters:_ 
+>  1  
+> _Sequences:_ 
+>  0
+
+> __Drop sequence__  
+> _Description:_ 
+> A sequence which takes a sequence and a shift parameter and is 
+> equivalent to shifting that particular sequence by the given amount.  
+> _Parameters:_ 
+> 1  
+> _Sequences:_ 
+> 1
+
+> __Fibonacci sequence__  
+> _Description:_ 
+> Fibonacci sequence starting with given parameters `zeroth` and `first`.  
+> _Parameters:_ 
+>  2  
+> _Sequences:_ 
+>  0
+
+> __Geometric sequence__  
+> _Description:_ 
+> Geometric sequence with two parameters: a start and a quotient.  
+> _Parameters:_ 
+>  2  
+> _Sequences:_ 
+>  0
+
+> __Linear Combination sequence__  
+> _Description:_ 
+> Linear combination of two sequences `a` and `b` as given by: `(x*a_i + y*b_i + w)_i` where `x`, `y` and `w` are the three parameters.  
+> _Parameters:_ 
+>  3  
+> _Sequences:_ 
+>  2
+
+> __Max sequence__  
+> _Description:_ 
+> A sequence that takes two sequences and uses their maximum term by term.  
+> _Parameters:_ 
+>  0  
+> _Sequences:_ 
+>  2
+
+> __Min sequence__  
+> _Description:_ 
+> A sequence that takes two sequences and uses their minimum term by term.  
+> _Parameters:_ 
+>  0  
+> _Sequences:_ 
+>  2
+
+> __Product sequence__  
+> _Description:_ 
+> A sequence that takes two sequences and multiplies them term by term.  
+> _Parameters:_ 
+>  0  
+> _Sequences:_ 
+>  2
+
+> __Sum sequence__  
+> _Description:_ 
+> A sequence that takes two sequences and adds them term by term.  
+> _Parameters:_ 
+>  0  
+> _Sequences:_ 
+>  2
 
 ### Authors:
 - Anara Nemanič
--- machine/src/functions/our_sequences.rs
+++ machine/src/functions/our_sequences.rs
@@ -11,15 +11,17 @@ pub fn our_sequences() -> Vec<SequenceInfo> {
         sequences: 0,
     });
     sequences.push(SequenceInfo {
+        name: "Average".to_string(),
+        description:
+            "A sequence that takes two sequences and calculates their average term by term."
+                .to_string(),
+        parameters: 0,
+        sequences: 2,
+    });
+    sequences.push(SequenceInfo {
         name: "Constant".to_string(),
         description: "Constant sequence with a single parameter: a value.".to_string(),
         parameters: 1,
-        sequences: 0,
-    });
-    sequences.push(SequenceInfo {
-        name: "Geometric".to_string(),
-        description: "Geometric sequence with two parameters: a start and a quotient.".to_string(),
-        parameters: 2,
         sequences: 0,
     });
     sequences.push(SequenceInfo {
@@ -31,8 +33,35 @@ pub fn our_sequences() -> Vec<SequenceInfo> {
         sequences: 1,
     });
     sequences.push(SequenceInfo {
-        name: "Sum".to_string(),
-        description: "A sequence that takes two sequences and adds them term by term.".to_string(),
+        name: "Fibonacci".to_string(),
+        description: "Fibonacci sequence starting with given parameters `zeroth` and `first`."
+            .to_string(),
+        parameters: 2,
+        sequences: 0,
+    });
+    sequences.push(SequenceInfo {
+        name: "Geometric".to_string(),
+        description: "Geometric sequence with two parameters: a start and a quotient.".to_string(),
+        parameters: 2,
+        sequences: 0,
+    });
+    sequences.push(SequenceInfo {
+        name: "LinearCombination".to_string(),
+        description: "Linear combination of two sequences `a` and `b` as given by: `(x*a_i + y*b_i + w)_i` where `x`, `y` and `w` are the three parameters.".to_string(),
+        parameters: 3,
+        sequences: 2,
+    });
+    sequences.push(SequenceInfo {
+        name: "Max".to_string(),
+        description: "A sequence that takes two sequences and uses their maximum term by term."
+            .to_string(),
+        parameters: 0,
+        sequences: 2,
+    });
+    sequences.push(SequenceInfo {
+        name: "Min".to_string(),
+        description: "A sequence that takes two sequences and uses their minimum term by term."
+            .to_string(),
         parameters: 0,
         sequences: 2,
     });
@@ -44,37 +73,8 @@ pub fn our_sequences() -> Vec<SequenceInfo> {
         sequences: 2,
     });
     sequences.push(SequenceInfo {
-        name: "Average".to_string(),
-        description:
-            "A sequence that takes two sequences and calculates their average term by term."
-                .to_string(),
-        parameters: 0,
-        sequences: 2,
-    });
-    sequences.push(SequenceInfo {
-        name: "Fibonacci".to_string(),
-        description: "Fibonacci sequence starting with given parameters `zeroth` and `first`."
-            .to_string(),
-        parameters: 2,
-        sequences: 0,
-    });
-    sequences.push(SequenceInfo {
-        name: "LinearCombination".to_string(),
-        description: "Linear combination of two sequences `a` and `b` as given by: `(x*a_i + y*b_i + w)_i` where `x`, `y` and `w` are the three parameters.".to_string(),
-        parameters: 3,
-        sequences: 2,
-    });
-    sequences.push(SequenceInfo {
-        name: "Min".to_string(),
-        description: "A sequence that takes two sequences and uses their minimum term by term."
-            .to_string(),
-        parameters: 0,
-        sequences: 2,
-    });
-    sequences.push(SequenceInfo {
-        name: "Max".to_string(),
-        description: "A sequence that takes two sequences and uses their maximum term by term."
-            .to_string(),
+        name: "Sum".to_string(),
+        description: "A sequence that takes two sequences and adds them term by term.".to_string(),
         parameters: 0,
         sequences: 2,
     });
