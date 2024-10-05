bfc0f7fd3ff3a597784109e10e4518df9dd8a03a
Luka Ponikvar <luka.ponikvar1@gmail.com>
2024-10-05 09:52:00 +0200

    Fibonacci se izracuna ucinkovito

--- machine/src/sequences/fibonacci.rs
+++ machine/src/sequences/fibonacci.rs
@@ -12,29 +12,29 @@ impl Fibonacci {
     pub fn new(zeroth: f64, first: f64) -> Box<Fibonacci> {
         Box::new(Fibonacci { zeroth, first })
     }
-
-    /// Returns `k_th` element of a given `Fibonacci` sequence.
-    fn k_th(&self, k: u64) -> f64 {
-        k_th(self.zeroth, self.first, k)
-    }
 }
 
 impl Sequence<f64> for Fibonacci {
     fn range(&self, range: &Range) -> Vec<f64> {
-        let mut result = vec![];
-        for index in range.from..range.to {
-            result.push(self.k_th(index));
+        let mut first = self.first;
+        let mut zeroth = self.zeroth;
+        let mut result: Vec<f64> = Vec::new();
+        if range.from == 0 && range.to > 0 {
+            result.push(zeroth);
         }
-        result
-    }
-}
-
-fn k_th(zeroth: f64, first: f64, k: u64) -> f64 {
-    if k == 0 {
-        return zeroth;
-    } else if k == 1 {
-        return first;
-    } else {
-        return k_th(first, first + zeroth, k - 1);
+        if range.from <= 1 && range.to > 1 {
+            result.push(first);
+        }
+        let mut i = 2;
+        while i < range.to {
+            let t = zeroth + first;
+            zeroth = first;
+            first = t;
+            if i >= range.from {
+                result.push(first);
+            }
+            i += 1;
+        }
+        return result;
     }
 }
